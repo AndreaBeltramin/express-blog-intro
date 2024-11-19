ab64b46ca882be1214e413a8ea2a2db5c5d8a2d6
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-19 16:54:45 +0100

    aggiunta commenti + asset statico + conteggio post

--- app.js
+++ app.js
@@ -2,45 +2,52 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("./", (req, res) => {
-	res.send("Server del mio blog");
+//configuro gli asset statici sull'applicazione in modo che si possano visualizzare le immagini associate ad ogni post
+app.use(express.static("public"));
+
+//creo il progetto base con una rotta / che ritorna un testo
+app.get("/", (req, res) => {
+	res.send("<h1>Server del mio blog</h1>");
 });
 
+//creo un array dove inserisco una lista di almeno 5 post, per ognuno indico titolo,
+//contenuto, immagine e tags sotto forma di array di stringhe
 const blogPosts = [
 	{
 		title: "ciambellone",
 		content: "ciambellone fatto in casa",
-		img: "./ciambellone.jpeg",
-		tags: [""],
+		img: "./images/ciambellone.jpeg",
+		tags: ["cucina", "dolce", "dessert"],
 	},
 	{
 		title: "cracker di barbabietola",
 		content: "cracker di barbabietola fatti in casa",
-		img: "./cracker_barbabietola.jpeg",
-		tags: [""],
+		img: "./images/cracker_barbabietola.jpeg",
+		tags: ["cucina", "antipasto"],
 	},
 	{
 		title: "pane dolce fritto",
 		content: "pane dolce fritto fatto in casa",
-		img: "./pane_fritto_dolce.jpeg",
-		tags: [""],
+		img: "./images/pane_fritto_dolce.jpeg",
+		tags: ["cucina", "panificazione", "dolce"],
 	},
 	{
 		title: "pasta alla barbabietola",
 		content: "pasta alla barbabietola fatta in casa",
-		img: "./pasta_barbabietola.jpeg",
-		tags: [""],
+		img: "./images/pasta_barbabietola.jpeg",
+		tags: ["cucina", "pasta", "primo"],
 	},
 	{
 		title: "torta paesana",
 		content: "torta paesana fatta in casa",
-		img: "./torta_paesana.jpeg",
-		tags: [""],
+		img: "./images/torta_paesana.jpeg",
+		tags: ["cucina", "dolce", "dessert"],
 	},
 ];
 
-app.get("./bacheca", (req, res) => {
-	res.json();
+//creo una rotta /bacheca che restituisce un oggetto json con la lista dei post e il conteggio partendo da un array
+app.get("/bacheca", (req, res) => {
+	res.json([blogPosts, "il numero di post è: " + blogPosts.length]);
 });
 
 app.listen(port, () => {
